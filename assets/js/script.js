4c18272d23b018a7f633a4788601983ede774b05
Lee Torson <torson77@gmail.com>
2018-06-29 20:55:19 +0100

    added on click scripts for modals, revealBtn, and modal close

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,42 @@ const Base = function init() {
   // Forces Correct Operating Context >= ES2015
 
   $(() => {
+
+    const $popupWindow = $('.js-popup');
+    let $modalTitle = $('.js-modal-title');
+    let $modalCopy = $('.js-modal-copy');
+    const $revealBtn = $('.js-reveal-btn');
+    const $revealElement = $('.js-reveal');
+
     $('.js-slider').slick({
       dots: true,
       arrows: true,
       slidesToShow: 1,
     });
+
+    $('.js-modal-open').on('click', function() {
+      let dataTitle = $(this).data("title"), dataCopy = $(this).data("copy");
+      $popupWindow.show();
+      $popupWindow.find($modalTitle).html(dataTitle);
+      $popupWindow.find($modalCopy).html(dataCopy);
+    });
+
+    $('.js-close').on('click', function() {
+      $popupWindow.hide();
+    });
+
+    $revealBtn.on('click', function(e) {
+      e.preventDefault();
+      let $this = $(this);
+      if(!$(this).hasClass('active')) {
+        $this.addClass('active').html('Close');
+        $revealElement.stop().slideDown();
+      } else {
+        $this.removeClass('active').html('Read More');
+        $revealElement.stop().slideUp();
+      }
+    });
+
   });
 };
 Base();
