701d82230b50c675553162370634f2b83253424f
Lee Torson <torson77@gmail.com>
2018-06-29 19:57:48 +0100

    Slick Slider init

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,11 @@ const Base = function init() {
   // Forces Correct Operating Context >= ES2015
 
   $(() => {
-
+    $('.js-slider').slick({
+      dots: true,
+      arrows: true,
+      slidesToShow: 1,
+    });
   });
 };
 Base();
