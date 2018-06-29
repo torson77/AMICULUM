a4f38020a7e3c9af13f1af946f2fbda381c39d6f
Lee Torson <torson77@gmail.com>
2018-06-29 20:57:20 +0100

    css and script base file updated

--- site/scripts/script.js
+++ site/scripts/script.js
@@ -42,10 +42,41 @@ var Base = function init() {
   // Forces Correct Operating Context >= ES2015
 
   $(function () {
+
+    var $popupWindow = $('.js-popup');
+    var $modalTitle = $('.js-modal-title');
+    var $modalCopy = $('.js-modal-copy');
+    var $revealBtn = $('.js-reveal-btn');
+    var $revealElement = $('.js-reveal');
+
     $('.js-slider').slick({
       dots: true,
       arrows: true,
       slidesToShow: 1
+    });
+
+    $('.js-modal-open').on('click', function () {
+      var dataTitle = $(this).data("title"),
+          dataCopy = $(this).data("copy");
+      $popupWindow.show();
+      $popupWindow.find($modalTitle).html(dataTitle);
+      $popupWindow.find($modalCopy).html(dataCopy);
+    });
+
+    $('.js-close').on('click', function () {
+      $popupWindow.hide();
+    });
+
+    $revealBtn.on('click', function (e) {
+      e.preventDefault();
+      var $this = $(this);
+      if (!$(this).hasClass('active')) {
+        $this.addClass('active').html('Close');
+        $revealElement.stop().slideDown();
+      } else {
+        $this.removeClass('active').html('Read More');
+        $revealElement.stop().slideUp();
+      }
     });
   });
 };
